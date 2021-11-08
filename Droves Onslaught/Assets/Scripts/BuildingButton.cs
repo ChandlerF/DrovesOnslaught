43c40de1e3ed474fe393f5f95a3080abd5bdb695
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2021-11-08 12:18:21 -0600

    Dynamic Upgrade Button System

--- Droves Onslaught/Assets/Scripts/BuildingButton.cs
+++ Droves Onslaught/Assets/Scripts/BuildingButton.cs
@@ -35,17 +35,34 @@ public class BuildingButton : MonoBehaviour
             //Enable building visual
             SelectedBuilding.GetComponent<ButtonInfo>().RangeVisual.GetComponent<SpriteRenderer>().enabled = true;
 
-
+             //Spawn Canvas (with buttons)
+             GameObject SpawnedCanvas = Instantiate(Buttons, transform.position, transform.rotation);
+                
             if (ButtonScript.BuildingUpgrades.Count > 0)
             {
-                //Spawn Canvas (with buttons)
-                GameObject SpawnedCanvas = Instantiate(Buttons, transform.position, transform.rotation);
-
                 GameObject ButtonParent = SpawnedCanvas.transform.GetChild(0).gameObject;
                 //Set Buttons Reference
                 GameObject ButtonOne = ButtonParent.transform.GetChild(0).gameObject;
                 GameObject ButtonTwo = ButtonParent.transform.GetChild(1).gameObject;
 
+
+                int i = 0;
+                //Set's upgrade buttons for buildings that have upgrades
+                //Have several buttons not active and per upgrade available set active appropriately
+                foreach(GameObject Button in ButtonScript.BuildingUpgrades)
+                {
+                    //Set each button to on
+                    Button.SetActive() = true;
+                    //SetButton Building
+                    Button.GetComponent<SetButton>().Building = ButtonScript.BuildingUpgrades[i];
+                    //Destroy SelectedBuilding
+                    Button.GetComponent<DestroyParent>().SelectedBuilding = SelectedBuilding;;
+                    
+                    i++;
+                }
+
+
+                /*
                 //Change button's building reference
                 ButtonOne.GetComponent<SetButton>().Building = ButtonScript.BuildingUpgrades[0];
 
@@ -54,14 +71,20 @@ public class BuildingButton : MonoBehaviour
                 //Change button's SelectedBuilding reference
                 ButtonOne.GetComponent<DestroyParent>().SelectedBuilding = SelectedBuilding;
                 ButtonTwo.GetComponent<DestroyParent>().SelectedBuilding = SelectedBuilding;
+                */
+            }
+            
+            
+            
 
                 //Sets the destroy Button on upgrade screen
                 ButtonParent.transform.GetChild(2).gameObject.GetComponent<DestroyParent>().SelectedBuilding = SelectedBuilding;
                 //Sets hide buttons button
                 ButtonParent.transform.GetChild(3).gameObject.GetComponent<DestroyParent>().SelectedBuilding = SelectedBuilding;
-
+                
                 ListScript.UpgradeButtonActive = true;
-            }
+                
+                
         }
         else if (ListScript.InTetherMode)   //If new building is selected while in tether mode:
         {
