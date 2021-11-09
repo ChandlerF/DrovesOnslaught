c0eb41e66302f53615e2f6d7d8429ed0f85eb26a
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2021-11-09 12:01:24 -0600

    Update Product.cs

--- Droves Onslaught/Assets/Scripts/Product.cs
+++ Droves Onslaught/Assets/Scripts/Product.cs
@@ -13,6 +13,7 @@ public class Product : MonoBehaviour
     //Feedback to know you're in Tether Mode (Selected Building's line renderer target is mouse or, because mobile, dim screen with overlay)
     //Add World Canvas to other buildings, like transport
     //Bug where if you're placing a building, and click another building instead of placing it, bad
+    //Can i cancel if I'm placing a building?
     
     //When placing a building, show all visuals for where to place it
     //Do this by setting value in building checker, have it go through dict and grab visuals from each object
